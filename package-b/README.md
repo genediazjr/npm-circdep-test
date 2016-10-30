a020f2d523cbfff3d4313e39c34ab1bff34fe26c
Gene Diaz <genediazjr@gmail.com>
2016-10-30 10:47:54 +0800

    add add package-b

---
+++ package-b/README.md
@@ -0,0 +1,4 @@
+# Package B
+Testing NPM Circular Dependency
+
+This package depends on package A which depends on this package.
---
+++ package-b/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const PackageA = require('npm-circdep-test-package-a');
+
+
+module.exports = `Message from package A: ${PackageA}`;
